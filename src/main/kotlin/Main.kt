6e4c15ba32ba952ed116e6a7addae3e67f98512e
youxingz <youxingzeta@gmail.com>
2024-02-04 13:11:25 +0800

    add title and update UI

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,14 +9,18 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowPlacement
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import kotlin.math.exp
 
 val _RED = Color(237, 106, 95)
@@ -46,6 +50,7 @@ fun RandomButton(
     members: MutableList<String>,
     triggerCount: Int,
     size: DpSize,
+    onReset: () -> Unit,
     onSelected: (String) -> Unit
 ) {
     var working by remember { mutableStateOf(false) }
@@ -57,31 +62,33 @@ fun RandomButton(
     val scope = rememberCoroutineScope()
 
     val triggerOne = suspend {
-        // 随机播放一秒
-        var index = 0
-        while (working && index < 10) {
-            delay(100)
-            member = rearrangedMembers[index++ % memberSize]
+        withContext(Dispatchers.IO) {
+            // 随机播放一秒
+            var index = 0
+            while (working && index < 10) {
+                delay(100)
+                member = rearrangedMembers[index++ % memberSize]
+            }
+            // target id:
+            val target = (memberSize / 2 + 1) % memberSize
+            memberIndex = (target - 5) % memberSize
+            var count = 0
+            while (working && count++ < 5) {
+                index = memberIndex++ % memberSize
+                member = rearrangedMembers[index]
+                delay((100 * exp(index * 1.2f / memberSize)).toLong())
+            }
+            onSelected(member)
+            // clear and rearrange.
+            rearrangedMembers.remove(member) // 去掉已中奖的人
+            rearrangedMembers = rearrange(rearrangedMembers).toMutableList()
         }
-        // target id:
-        val target = (memberSize / 2 + 1) % memberSize
-        memberIndex = (target - 5) % memberSize
-        var count = 0
-        while (working && count++ < 5) {
-            index = memberIndex++ % memberSize
-            member = rearrangedMembers[index]
-            delay((100 * exp(index * 1.2f / memberSize)).toLong())
-        }
-        onSelected(member)
-        // clear and rearrange.
-        rearrangedMembers.remove(member) // 去掉已中奖的人
-        rearrangedMembers = rearrange(rearrangedMembers).toMutableList()
     }
 
-    Row {
+    Row() {
         if (working) {
             OutlinedButton(
-                modifier = Modifier.size(size.width * 2, size.height),
+                modifier = Modifier.size(size.width * 2, size.height).padding(24.dp),
                 colors = ButtonDefaults.outlinedButtonColors(
                     backgroundColor = _RED,
                     contentColor = Color.White,
@@ -99,12 +106,13 @@ fun RandomButton(
             if (members.size - triggerCount >= memberSize) {
 
                 OutlinedButton(
-                    modifier = Modifier.size(size.width * 2, size.height),
+                    modifier = Modifier.size(size.width * 2, size.height).padding(24.dp),
                     colors = ButtonDefaults.outlinedButtonColors(
                         backgroundColor = _RED,
                         contentColor = Color.White,
                     ),
                     onClick = {
+                        onReset()
                         rearrangedMembers = rearrange(members).toMutableList()
                     }) {
                     Text(
@@ -115,7 +123,7 @@ fun RandomButton(
                 }
             } else {
                 OutlinedButton(
-                    modifier = Modifier.size(size.width * 2, size.height),
+                    modifier = Modifier.size(size.width * 2, size.height).padding(24.dp),
                     colors = ButtonDefaults.outlinedButtonColors(
                         backgroundColor = _RED,
                         contentColor = Color.White,
@@ -125,13 +133,15 @@ fun RandomButton(
                             return@OutlinedButton
                         }
                         scope.launch {
-                            working = true
-                            var i = 0
-                            while (i++ < triggerCount) {
-                                triggerOne()
-                                delay(1000)
+                            withContext(Dispatchers.IO) {
+                                working = true
+                                var i = 0
+                                while (i++ < triggerCount) {
+                                    triggerOne()
+                                    delay(1000)
+                                }
+                                working = false
                             }
-                            working = false
                         }
                     }) {
                     Text(
@@ -146,7 +156,13 @@ fun RandomButton(
 }
 
 @Composable
-fun CardN(members: MutableList<String>, triggerCount: Int, itemSize: DpSize, onNext: (List<String>) -> Unit) {
+fun CardN(
+    title: String,
+    members: MutableList<String>,
+    triggerCount: Int,
+    itemSize: DpSize,
+    onNext: (List<String>) -> Unit
+) {
 
     val selectedMembers = remember { mutableStateListOf<String>() }
 
@@ -156,48 +172,70 @@ fun CardN(members: MutableList<String>, triggerCount: Int, itemSize: DpSize, onN
         verticalArrangement = Arrangement.Center,
     ) {
         Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier.height(itemSize.height),
+            horizontalArrangement = Arrangement.Center,
         ) {
-            for (j in 0 until triggerCount) {
-                Box(modifier = Modifier.size(itemSize), contentAlignment = Alignment.Center) {
-                    val index = j
-                    if (index < selectedMembers.size) {
+            Text(title, fontSize = 72.sp, fontWeight = FontWeight.Bold)
+        }
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+        ) {
+            if (triggerCount == 1) {
+                Box(modifier = Modifier.size(itemSize.width * 2, itemSize.height), contentAlignment = Alignment.Center) {
+                    if (selectedMembers.isNotEmpty()) {
                         Text(
-                            text = selectedMembers[index],
-                            fontSize = 72.sp,
+                            text = selectedMembers[0],
+                            fontSize = 156.sp,
                             fontWeight = FontWeight.Bold,
+                            overflow = TextOverflow.Visible,
+                            maxLines = 1,
                         )
+                    }
+                }
+            } else {
+                for (j in 0 until triggerCount) {
+                    Box(modifier = Modifier.size(itemSize), contentAlignment = Alignment.Center) {
+                        val index = j
+                        if (index < selectedMembers.size) {
+                            Text(
+                                text = selectedMembers[index],
+                                fontSize = 86.sp,
+                                fontWeight = FontWeight.Bold,
+                                overflow = TextOverflow.Visible,
+                                maxLines = 1,
+                            )
+                        }
                     }
                 }
             }
         }
-        Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically,
-            ) {
-                RandomButton(members = members, triggerCount = triggerCount, size = itemSize) {
-                    println("Selected: $it")
-                    if (selectedMembers.contains(it)) return@RandomButton
-                    selectedMembers.add(it)
-                }
-                if (selectedMembers.size >= triggerCount) {
-                    OutlinedButton(
-                        modifier = Modifier.size(itemSize.width * 2, itemSize.height),
-                        colors = ButtonDefaults.outlinedButtonColors(
-                            backgroundColor = _RED,
-                            contentColor = Color.White,
-                        ),
-                        onClick = { onNext(selectedMembers) }
-                    ) {
-                        Text(
-                            "下一轮",
-                            fontSize = 96.sp,
-                            fontWeight = FontWeight.Bold,
-                        )
-                    }
+        Row(
+            modifier = Modifier.fillMaxWidth().height(itemSize.height * 3),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            RandomButton(members = members, triggerCount = triggerCount, size = itemSize, onReset = {
+                selectedMembers.clear()
+            }) {
+                println("Selected: $it")
+                if (selectedMembers.contains(it)) return@RandomButton
+                selectedMembers.add(it)
+            }
+            if (triggerCount > 1 && selectedMembers.size >= triggerCount) {
+                OutlinedButton(
+                    modifier = Modifier.size(itemSize.width * 2, itemSize.height).padding(24.dp),
+                    colors = ButtonDefaults.outlinedButtonColors(
+                        backgroundColor = _RED,
+                        contentColor = Color.White,
+                    ),
+                    onClick = { onNext(selectedMembers) }
+                ) {
+                    Text(
+                        "下一轮",
+                        fontSize = 96.sp,
+                        fontWeight = FontWeight.Bold,
+                    )
                 }
             }
         }
@@ -230,7 +268,9 @@ fun Card20(members: MutableList<String>, itemSize: DpSize, onNext: (List<String>
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.Center,
         ) {
-            RandomButton(members = members, triggerCount = 20, size = itemSize) {
+            RandomButton(members = members, triggerCount = 20, size = itemSize, onReset = {
+                selectedMembers.clear()
+            }) {
                 println("Selected: $it")
                 if (selectedMembers.contains(it)) return@RandomButton
                 selectedMembers.add(it)
@@ -276,7 +316,7 @@ fun Card20(members: MutableList<String>, itemSize: DpSize, onNext: (List<String>
 fun App(size: DpSize) {
     var text by remember { mutableStateOf("Hello, World!") }
     val members by remember { mutableStateOf(MEMBERS.toMutableList()) }
-    var step by remember { mutableStateOf(Step.Lucky) }
+    var step by remember { mutableStateOf(Step.First) }
 
     MaterialTheme {
         when (step) {
@@ -287,25 +327,45 @@ fun App(size: DpSize) {
                 }
 
             Step.Forth ->
-                CardN(members = members.toMutableList(), triggerCount = 4, size / 5) {
+                CardN(
+                    "\uD83C\uDFC5\uFE0F四等奖\uD83C\uDFC5\uFE0F",
+                    members = members.toMutableList(),
+                    triggerCount = 4,
+                    size / 5
+                ) {
                     step = Step.Third
                     members.removeAll(it)
                 }
 
             Step.Third ->
-                CardN(members = members.toMutableList(), triggerCount = 3, size / 5) {
+                CardN(
+                    "\uD83C\uDFC5\uFE0F三等奖\uD83C\uDFC5\uFE0F",
+                    members = members.toMutableList(),
+                    triggerCount = 3,
+                    size / 5
+                ) {
                     step = Step.Second
                     members.removeAll(it)
                 }
 
             Step.Second ->
-                CardN(members = members.toMutableList(), triggerCount = 2, size / 5) {
+                CardN(
+                    "\uD83C\uDFC5\uFE0F二等奖\uD83C\uDFC5\uFE0F",
+                    members = members.toMutableList(),
+                    triggerCount = 2,
+                    size / 5
+                ) {
                     step = Step.First
                     members.removeAll(it)
                 }
 
             Step.First ->
-                CardN(members = members.toMutableList(), triggerCount = 1, size / 5) {
+                CardN(
+                    "\uD83C\uDFC5\uFE0F一等奖\uD83C\uDFC5\uFE0F",
+                    members = members.toMutableList(),
+                    triggerCount = 1,
+                    size / 5
+                ) {
                     members.removeAll(it)
                 }
         }
